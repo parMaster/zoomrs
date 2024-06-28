73c1a41cf1ce1d225906f40c9d7d65f5ada6d01d
parmaster <tony.racomore@gmail.com>
2024-06-28 14:29:51 +0300

    gziping backups saves ~75% space

--- dist/backup_db.sh
+++ dist/backup_db.sh
@@ -4,11 +4,15 @@
 # Get the date and time
 now=$(date +"%Y-%m-%d_%H-%M-%S")
 
+echo "Backing up database to $HOME/backups/$now.tar.gz"
+
 # Create a folder with the date and time
 mkdir -p $HOME/backups/$now
 
 # Copy the database to the folder
 cp -r /data/_db $HOME/backups/$now
+
+cd $HOME/backups/ && tar -czf "${now}.tar.gz" "${now}" && rm -rf "${now}"
 
 # cron job to run this script every day at 10am
 # 0 10 * * * sh $HOME/go/src/zoomrs/backup_db.sh
